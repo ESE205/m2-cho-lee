903413dd67ff9e28f113f804aef9869720e63deb
swv7979 <cho.jesse@wustl.edu>
2024-01-26 16:12:39 -0600

    Program1

--- blink_n_times.py
+++ blink_n_times.py
@@ -8,12 +8,17 @@ if (len(sys.argv) > 1): n = int(sys.argv[1])
 if (len(sys.argv) == 1): n = 5
 
 pin1 = 11
+pin2 = 15
 GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)   
+GPIO.setup(pin2, GPIO.IN)
 
-while n > 0: # Run n times
-   n -= 1 # Decrement counter
-   GPIO.output(pin1, GPIO.HIGH) # Turn on
-   sleep(1)                     # Sleep for 1 second
-   GPIO.output(pin1, GPIO.LOW)  # Turn off
-   sleep(1)                     # Sleep for 1 second
+while True:
+   if GPIO.input(pin2):
+      while n > 0: # Run n times
+         n -= 1 # Decrement counter
+         GPIO.output(pin1, GPIO.HIGH) # Turn on
+         sleep(1)                     # Sleep for 1 second
+         GPIO.output(pin1, GPIO.LOW)  # Turn off
+         sleep(1)                     # Sleep for 1 second
+      break
 GPIO.cleanup()
