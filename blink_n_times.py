e67f261d60283d4378391ad396da54ecf93f9f93
swv7979 <cho.jesse@wustl.edu>
2024-01-26 14:23:59 -0600

    Blink n times

--- blink_n_times.py
+++ blink_n_times.py
@@ -1,15 +1,17 @@
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep from time module
+import sys
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 
-ITER_COUNT = 15  
-pin1 = 11
+if (len(sys.argv) > 1): n = int(sys.argv[1])
+if (len(sys.argv) == 1): n = 5
 
+pin1 = 11
 GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)   
 
-while ITER_COUNT > 0: # Run ITER_COUNT times
-   ITER_COUNT -= 1 # Decrement counter
+while n > 0: # Run n times
+   n -= 1 # Decrement counter
    GPIO.output(pin1, GPIO.HIGH) # Turn on
    sleep(1)                     # Sleep for 1 second
    GPIO.output(pin1, GPIO.LOW)  # Turn off
