c064bf396c29441ffe2e86fb0baea81b2188cc07
swv7979 <cho.jesse@wustl.edu>
2024-01-26 21:18:17 -0600

    fdj

--- blink_with_write.py
+++ blink_with_write.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 import time
 import sys
+from time import sleep
 from datetime import datetime
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
@@ -14,7 +15,7 @@ debug = False
 if '-debug' in sys.argv:
     debug = True
 
-blinkrate = int(input('Enter the blink rate:'))
+blinkrate = int(input('Enter the blink rate: '))
 max_time = int(input('Enter the amount of time you want to run this in seconds: '))
 iteration = 0
 
@@ -23,14 +24,14 @@ with open('data.txt', 'w') as data:
     while (time.time() - start_time) < max_time: 
         if GPIO.input(pin2):
             GPIO.output(pin1, GPIO.HIGH) # Turn on
-            data.write(f,{'time.time():1.0f}\t{GPIO.input(pin2)}\n')
+            data.write(f'{time.time():1.0f}\t{GPIO.input(pin2)}\n')
             sleep(blinkrate)                     
             GPIO.output(pin1, GPIO.LOW)  # Turn off
-            data.write(f,{'time.time():1.0f}\t{GPIO.input(pin2)}\n')
+            data.write(f'{time.time():1.0f}\t{GPIO.input(pin2)}\n')
             sleep(blinkrate)
             iteration += 1
         if debug: 
             date_t = datetime.fromtimestamp(time.time())
-            print(f,'Formatted Date : {date_t.strftime('%m_%d_%Y_%H_%M_%S')}\tNumber of Iteration : {iteration}\tSwitch State: {GPIO.input(pin2)}\n')
+            print(f'Formatted Date : {date_t}\tNumber of Iteration : {iteration}\tSwitch State: {GPIO.input(pin2)}\n')
                
     
